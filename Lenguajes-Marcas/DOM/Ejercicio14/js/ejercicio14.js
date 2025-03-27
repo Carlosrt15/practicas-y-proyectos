7dbabc55c6fbd9ac8c8a2e0b83810880750f45d9
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-03-27 17:19:11 +0100

    Fallos en el DOM

--- Lenguajes-Marcas/DOM/Ejercicio14/js/ejercicio14.js
+++ Lenguajes-Marcas/DOM/Ejercicio14/js/ejercicio14.js
@@ -1,4 +1,4 @@
-window.onload = function ejercicio14(){
+function ejercicio14(){
 
     let pedirEdad = Number(prompt("Introduce tu edad"));
 
@@ -17,27 +17,76 @@ window.onload = function ejercicio14(){
 
             form.insertBefore(crearNumCuenta, edadInput.nextElementSibling);
             form.insertBefore(inputNumCuenta, crearNumCuenta.nextElementSibling);
-
-            // Contraseña se pone bluur
-            let passwordInput = document.getElementById("password");
-
-            if (passwordInput) {
-
-                passwordInput.addEventListener("focus", function () {
-                    passwordInput.classList.add("resaltado"); // aplica la clase
-                    
-                });
-
-                // Restaura el borde
-
-                passwordInput.addEventListener("blur", function() {
-
-                    passwordInput.classList.remove("resaltado");
-                });
-            }
- 
-
-            
+                        
         }
 
+
+         // Contraseña se pone bluur
+         let passwordInput = document.getElementById("password");
+
+         if (passwordInput) {
+
+             passwordInput.addEventListener("focus", function () {
+                 passwordInput.classList.add("resaltado"); // aplica la clase
+                 
+             });
+
+             // Restaura el borde
+
+             passwordInput.addEventListener("blur", function() {
+
+                 passwordInput.classList.remove("resaltado");
+             });
+         }
+
+         
+
+         // aumentar el nombre de los campos
+         let labels = document.getElementsByClassName("label");
+
+         const labelsArray = Array.from(labels);
+         
+
+         for(let i = 0; i < labelsArray.length; i++) {
+            const label = labelsArray[i];
+
+            label.addEventListener("mouseenter", () => {
+                label.classList.add("label-hover");
+
+            });
+
+                              
+           
+         }
+
+
+
+
+         
+
 }
+
+
+function llamarFormulario() {
+    const formEnvio = document.getElementById("formenvio");
+    
+    formEnvio.addEventListener("submit", function(event) {
+        // Prevenir el envío automático
+        event.preventDefault();
+        
+        // Pedir confirmación
+        const pedirConfirmacion = confirm("¿Estás seguro de querer enviar el formulario?");
+        console.log(pedirConfirmacion); // true (Aceptar) o false (Cancelar)
+        
+        if (pedirConfirmacion) {
+            
+            this.submit();
+        }
+        
+    });
+}
+
+
+
+
+

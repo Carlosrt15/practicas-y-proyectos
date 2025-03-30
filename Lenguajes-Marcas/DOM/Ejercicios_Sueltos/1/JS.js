f4a4a0119796ba5e6c64318acc321a3330751881
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-03-30 19:20:03 +0200

    Haciendo ejercicios de JS

---
+++ Lenguajes-Marcas/DOM/Ejercicios_Sueltos/1/JS.js
@@ -0,0 +1,32 @@
+function arrayAleatorio() {
+
+    let lista = [];
+    
+    
+    while(lista.length < 10) {
+        let numerosAletorios = Math.round(Math.random()*100);
+        console.log(numerosAletorios);
+        lista.push(numerosAletorios)
+    }
+
+
+    console.log(lista+" Desde fuera");
+    let perdirNumero = Number(prompt("Introduce un numero (Intenta adivinar el numero)"));
+
+
+    if(lista.includes(perdirNumero)) {
+
+        alert("Bien acertaste un numero!!!!!");
+    } else {
+        alert(lista[2]);
+    }
+
+
+}
+
+function arrayIngredientes() {
+
+    let listaIngredientes = ["Pan","Aceite","Olivas","Pimiento","Azucar"];
+
+    alert(listaIngredientes+" La pizza Perfecta");
+}

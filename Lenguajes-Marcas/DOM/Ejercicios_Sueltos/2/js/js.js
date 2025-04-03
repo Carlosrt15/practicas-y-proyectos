12ab8fe77e452dd0b3d7597eac438cd78d93a259
Carlos201120 <carlos@carlos>
2025-04-03 11:43:59 +0200

    Ejercico de repaso

---
+++ Lenguajes-Marcas/DOM/Ejercicios_Sueltos/2/js/js.js
@@ -0,0 +1,35 @@
+function arrayEnDos(){
+
+let pares = [];
+let impares = [];
+
+
+
+
+for (let i = 0; i < 3; i++) {
+    let pedirNumeros = Number(prompt("Introduce un numero del 1 al 100 :"+i+ "<= numeros introducidos"));
+
+    if(pedirNumeros %2 == 0) {
+        pares.push(pedirNumeros);
+
+    } else {
+
+        impares.push(pedirNumeros);
+    }
+
+}
+console.log(pares);
+console.log(impares);
+
+alert(pares.length+" pares");
+alert(impares.length+" impares");
+
+
+
+
+}
+
+
+function limaOnclick() {
+    
+}

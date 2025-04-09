5becdb6a8161b05f2bd0b89334188e986a9b37a9
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2025-04-09 20:32:44 +0200

    Señor de los anillos 1/2

---
+++ Lenguajes-Marcas/DOM/Repaso_antes-exam/práctica JavaScript_Señor_anillos/src/js/lotr.js
@@ -0,0 +1,107 @@
+function cambiarColor() {
+
+    //Añade el color
+let cambiarColor1 = document.getElementById("1");
+cambiarColor1.setAttribute("class","cambiarColor");
+
+    //Quitar el color 
+   let quitarColor1 = document.getElementById("1");
+
+   // el bind crea como una
+   quitarColor1.addEventListener("mouseleave", quitarColor1.removeAttribute.bind(quitarColor1, "class"));
+
+
+}
+
+
+function cambiarColor2() {
+
+    //Añade el color
+let cambiarColor2 = document.getElementById("2");
+cambiarColor2.setAttribute("class","cambiarColor");
+
+    //Quitar el color 
+   let quitarColor2 = document.getElementById("2");
+
+   // el bind crea como una
+   quitarColor2.addEventListener("mouseleave", quitarColor2.removeAttribute.bind(quitarColor2, "class"));
+
+
+}
+
+
+function cambiarColor3() {
+
+    //Añade el color
+let cambiarColor3 = document.getElementById("3");
+cambiarColor3.setAttribute("class","cambiarColor");
+
+    //Quitar el color 
+   let quitarColor3 = document.getElementById("3");
+
+   // el bind crea como una funcion nueva
+   quitarColor3.addEventListener("mouseleave", quitarColor3.removeAttribute.bind(quitarColor3, "class"));
+
+
+}
+
+
+function cambiarColor4() {
+
+    //Añade el color
+let cambiarColor4 = document.getElementById("4");
+cambiarColor4.setAttribute("class","cambiarColor");
+
+    //Quitar el color 
+   let quitarColor4 = document.getElementById("4");
+
+   // el bind crea como una funcion nueva
+   quitarColor4.addEventListener("mouseleave", quitarColor4.removeAttribute.bind(quitarColor4, "class"));
+
+
+}
+
+
+
+function cambiarColor5() {
+
+    //Añade el color
+let cambiarColor5 = document.getElementById("5");
+cambiarColor5.setAttribute("class","cambiarColor");
+
+    //Quitar el color 
+   let quitarColor5 = document.getElementById("5");
+
+   // el bind crea como una funcion nueva
+   quitarColor5.addEventListener("mouseleave", quitarColor5.removeAttribute.bind(quitarColor5, "class"));
+
+
+}
+
+
+
+
+function cambiarColor6() {
+
+    //Añade el color
+let cambiarColor6 = document.getElementById("6");
+cambiarColor6.setAttribute("class","cambiarColor");
+
+    //Quitar el color 
+   let quitarColor5 = document.getElementById("6");
+
+   // el bind crea como una funcion nueva
+   quitarColor6.addEventListener("mouseleave", quitarColor6.removeAttribute.bind(quitarColor6, "class"));
+
+
+}
+
+
+
+// funcion tabla
+
+function insertarLibros() {
+
+    
+
+}

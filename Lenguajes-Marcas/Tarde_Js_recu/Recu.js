daf4f4760c5b0ed9725f3811079fbb5f0e96c465
unknown <practicas1@FPB03.CALASANZSALAMANCA.local>
2025-05-12 13:03:49 +0200

    mañana antes de exam

--- Lenguajes-Marcas/Tarde_Js_recu/Recu.js
+++ Lenguajes-Marcas/Tarde_Js_recu/Recu.js
@@ -1,10 +1,13 @@
 function calcular() {
     let pedirNumero = Number(prompt("Introduce un numero"));
     let resultado = "";
+    let impar = [];
 
     for (let i = 0; i <= pedirNumero; i++) {
         if (i % 2 === 0) {
             resultado += i + " ";
+        } else {
+
         }
     }
     console.log(resultado);

f91a99c1d3e39784669160d49d7e788fe45c5477
unknown <practicas1@FPB03.CALASANZSALAMANCA.local>
2025-05-16 13:48:21 +0200

    1gym

--- Angular/Practicas_1--master/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-// app.routes.ts
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
-  }
-];
